f968e52e1be3708b6af2ef4331726be40a7b2ae6
Sergio <sbouden16@gmail.com>
2021-05-14 14:03:04 +0200

    Add class method

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,17 @@ const form = document.querySelector('form');
 const addBook = document.querySelector('.add-book');
 // FUNCTIONS
 
-const Book = (title, author, pages, read) => ({
-  title, author, pages, read,
-});
+const Book = class {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+};
 
 function addBookToLibrary(title, author, pages, read) {
-  const book = Book(title, author, pages, read);
+  const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
 
